0ed8c69307688bbadf7a0ac82285eeafc8ac14a6
Max Alva <alva85@gmail.com>
2018-07-10 16:21:15 -0500

    Desplegar lista de TODOS

--- src/app/todo/todos-list/todos-list.component.ts
+++ src/app/todo/todos-list/todos-list.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../app.reducers';
+import { Todo } from '../models/todo.model';
 
 @Component({
-  selector: 'app-todos-list',
-  templateUrl: './todos-list.component.html',
-  styles: []
+    selector: 'app-todos-list',
+    templateUrl: './todos-list.component.html',
+    styles: []
 })
 export class TodosListComponent implements OnInit {
 
-  constructor() { }
+    todos: Todo[] = [];
 
-  ngOnInit() {
-  }
+    constructor(private store: Store<AppState>) { }
+
+    ngOnInit() {
+        this.store.subscribe(state => {
+            this.todos = state.todos;
+        });
+    }
 
 }
