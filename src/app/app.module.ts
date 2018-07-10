5cdd51cb4588cd8d555a913ab6a91c8d941227c5
Max Alva <alva85@gmail.com>
2018-07-10 16:16:31 -0500

    Agregar un TODO

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
@@ -26,6 +27,7 @@ import { environment } from '../environments/environment';
     ],
     imports: [
         BrowserModule,
+        ReactiveFormsModule,
         StoreModule.forRoot({ todos: todoReducer }),
         StoreDevtoolsModule.instrument({
             maxAge: 25,
