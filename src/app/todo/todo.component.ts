4efd4d4aa466c93efdba4b758c9080b204221391
Max Alva <alva85@gmail.com>
2018-07-12 21:31:20 -0500

    Marcar o desmarcar todos

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { ToggleAllTodoAction } from './todo.actions';
+import { Store } from '@ngrx/store';
+import { AppState } from '../app.reducers';
 
 @Component({
-  selector: 'app-todo',
-  templateUrl: './todo.component.html',
-  styles: []
+    selector: 'app-todo',
+    templateUrl: './todo.component.html',
+    styles: []
 })
 export class TodoComponent implements OnInit {
 
-  constructor() { }
+    completado = false;
 
-  ngOnInit() {
-  }
+    constructor(private store: Store<AppState>) { }
+
+    ngOnInit() {
+    }
+
+    toggleAll() {
+        this.completado = !this.completado;
+
+        const action = new ToggleAllTodoAction(this.completado);
+        this.store.dispatch(action);
+
+    }
 
 }
